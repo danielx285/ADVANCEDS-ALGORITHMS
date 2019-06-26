6eefdac819c76755be9bb4473a098d0416adf65f
Daniel Melo <38673841+danielx285@users.noreply.github.com>
2019-06-26 01:10:33 -0300

    Create Combinatorics.cpp

---
+++ Mathematics/Combinatorics.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define MOD int(1e9) + 7 
+
+using namespace std;
+
+int exp(int a, int b){
+	if(b == 0) return 1;
+	if(b % 2 == 0){
+		li x = exp(a, b/2);
+		return (x  *  x ) % MOD; 
+	}
+	return (exp(a, b-1) * (a) % MOD;
+}
+
+//Fermat's little theorem to inverse modular
+int DivMod(a, b){
+    return (a * exp(b, MOD - 2)) % MOD;
+}
+
+int PgSum(int a1 , int q, int n){
+    return DivMod( a1*(exp(q, n) -1 ) % MOD , q - 1)  ;
+}
+
+int main(){
+  cout << PgSum(3, 2, 4) << endl;
+}
